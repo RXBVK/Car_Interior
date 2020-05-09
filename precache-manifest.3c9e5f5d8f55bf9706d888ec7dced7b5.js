4a84f12757f89e02aad4f43f0a92ca4cf6c59553
RXBVK <52883785+RXBVK@users.noreply.github.com>
2020-05-09 20:16:55 +0200

    Updates

---
+++ precache-manifest.3c9e5f5d8f55bf9706d888ec7dced7b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7af260da22f5b639d0ca5f39bb4533",
+    "url": "/RT-Main-Task/index.html"
+  },
+  {
+    "revision": "0aff4a3a9d99275e0230",
+    "url": "/RT-Main-Task/static/css/main.a1076b1d.chunk.css"
+  },
+  {
+    "revision": "5b4bec306f7117c2e1d9",
+    "url": "/RT-Main-Task/static/js/2.8a47ad00.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/RT-Main-Task/static/js/2.8a47ad00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aff4a3a9d99275e0230",
+    "url": "/RT-Main-Task/static/js/main.07e83422.chunk.js"
+  },
+  {
+    "revision": "daf4fa538e5a17973ad7",
+    "url": "/RT-Main-Task/static/js/runtime-main.7d80a31c.js"
+  }
+]);
